b741326b2dd274a58266b7ce8d3222c0c8fb91dc
Michael <mjrharrison001@gmail.com>
2017-06-19 21:24:43 -0400

    render fix

--- app.js
+++ app.js
@@ -43,7 +43,6 @@ app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
-    res.setHeader('content-type', 'application/json');
     next();
 });
 
